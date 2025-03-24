0ac1552e65fd33b46e0d817d8f8e78a993777e17
EEVAEP <eeva.ep@techversantinfotech.com>
2025-03-24 14:21:42 +0530

    Todo List

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const[count, setCount] = useState(0);
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
   return (
-    <div style={{textAlign:'center', marginTop: '50px'}}>
-      <h1>Counter: {count} </h1>
-      <button onClick= {() => setCount(count + 1)}>Increment</button>
-      <br></br><br></br>
-      <button onClick= {() => setCount(count -1)}>Decrement</button>
-      <br></br><br></br>
-      <button onClick= {() => setCount(0)}>Reset</button>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <button onClick={handleAddTask}>Add Task</button>
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
